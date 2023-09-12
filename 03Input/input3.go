978235b34cf2426123e89e2ea4ed7d098acadc16
Hacker <hacker@gmail.com>
2023-09-12 12:34:56 +0530

    test

--- 03Input/input3.go
+++ 03Input/input3.go
@@ -13,6 +13,10 @@ func main() {
 
 	// Read user input from the standard input (keyboard)
 	_, err := fmt.Scan(&name)
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return
+	}
 
 	// Display the user's input
 	fmt.Println("Hello,", name, "! Nice to meet you.")
